31b6060ef1a349f8755dfa38f17e808b6f2892b4
killian <killianjarelvitug@yahoo.com>
2023-09-15 11:22:15 +0800

    Add project files.

---
+++ PDC60-Act2/PDC60-Act2/View/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PDC60.ViewModel;
+
+namespace PDC60.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new TaskViewModel();
+        }
+    }
+}
---
+++ PDC60-Act2/PDC60-Act2/ViewModel/TaskViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PDC60.Model;
+
+namespace PDC60.ViewModel
+{
+    internal class TaskViewModel
+    {
+        public TaskModel TaskModelSet { get; set; }
+
+        public TaskViewModel()
+        {
+            TaskModelSet = new TaskModel
+            {
+                Subjectcode = "PDC06",
+                SubjectTitle = "Xamarin Mobile Programming",
+                Unit = 3,
+            };
+        }
+    }
+}
